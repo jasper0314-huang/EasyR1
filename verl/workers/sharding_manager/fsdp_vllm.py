8cbff92e2df678b34844cd15491638bf3efbbe45
jasper0314-huang <q0982569154@gmail.com>
2025-12-28 06:17:26 -0800

    [deps] use get_tp_group for vLLM >= 0.12.0 compatibility

--- verl/workers/sharding_manager/fsdp_vllm.py
+++ verl/workers/sharding_manager/fsdp_vllm.py
@@ -49,7 +49,10 @@ class FSDPVLLMShardingManager(BaseShardingManager):
         self.world_size = dist.get_world_size()
         self.tp_size = vllm_ps.get_tensor_model_parallel_world_size()
         self.tp_rank = vllm_ps.get_tensor_model_parallel_rank()
-        self.tp_group = vllm_ps.get_tensor_model_parallel_group().device_group
+        try:
+            self.tp_group = vllm_ps.get_tp_group().device_group
+        except:
+            self.tp_group = vllm_ps.get_tensor_model_parallel_group().device_group  # vllm<0.12.0
 
         # Record freed bytes to estimate memory usage correctly
         # https://github.com/vllm-project/vllm/pull/11743#issuecomment-2754338119
